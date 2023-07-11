6b0b0ee82e75ad0e73f4eaa4d7f392454db62b0c
Dag Königrör <72100882+dkoenigroer@users.noreply.github.com>
2023-07-11 12:08:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from djangocms_snippet import __version__
 
 REQUIREMENTS = [
     'django-cms>=3.7',
-    'django-treebeard>=4.3,<4.5',
+    'django-treebeard>=4.3,<4.7',
 ]
 
 EXTRA_REQUIREMENTS = {
